10ed0f4d898d5b1c1e29fe72a4690d1b7644f20e
misoa <misoarakotoniaina@gmail.com>
2026-08-27 02:26:02 +0300

    feat: add in examen

--- src/models/Examen.js
+++ src/models/Examen.js
@@ -3,5 +3,7 @@ import axiosClient from '../api/axiosClient';
 export const Examen = {
   getAllExams: () => axiosClient.get('/exams'),
   getExamById: (id) => axiosClient.get(`/exams/${id}`),
-  submitExamAnswers: (id, answers) => axiosClient.post(`/exams/${id}/submit`, answers)
+  create: (data) => axiosClient.post('/exams', data),
+  addQuestion: (examId, questionData) => axiosClient.post(`/exams/${examId}/questions`, questionData),
+  delete: (id) => axiosClient.delete(`/exams/${id}`),
 };
