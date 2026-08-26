81a248288fe606bd2f8c105a9225d9e3aa37b271
misoa <misoarakotoniaina@gmail.com>
2026-08-27 02:37:39 +0300

    feat: Page d'erreur not found

---
+++ src/views/page/PageErreur.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function NotFoundPage() {
+  return (
+    <div className="vh-100 d-flex flex-column align-items-center justify-content-center bg-light">
+      <h1 className="display-1 fw-bold text-secondary">404</h1>
+      <p className="lead">Page introuvable</p>
+      <Link to="/" className="btn btn-primary">Retour à l'accueil</Link>
+    </div>
+  );
+}
