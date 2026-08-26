e5761d03001ffc14f8cadc0550691cb1b4e06a27
misoa <misoarakotoniaina@gmail.com>
2026-08-27 02:18:57 +0300

    fix: user

--- src/models/User.js
+++
@@ -1,7 +0,0 @@
-import axiosClient from '../api/axiosClient';
-
-export const userModel = {
-  getAllStudents: () => axiosClient.get('/users/students'),
-  createStudent: (data) => axiosClient.post('/users/students', data),
-  deleteStudent: (id) => axiosClient.delete(`/users/students/${id}`)
-};
