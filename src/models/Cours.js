a41a48d325ebc750b40fd5a77c6cc8f5b11c1980
misoa <misoarakotoniaina@gmail.com>
2026-08-28 15:41:25 +0300

    delete: CoursController et CoursModel

--- src/models/Cours.js
+++
@@ -1,8 +0,0 @@
-import axiosClient from '../api/axiosClient';
-
-export const Cours = {
-  getAll: () => axiosClient.get('/courses'),
-  getById: (id) => axiosClient.get(`/courses/${id}`),
-  create: (data) => axiosClient.post('/courses', data),
-  delete: (id) => axiosClient.delete(`/courses/${id}`),
-};
